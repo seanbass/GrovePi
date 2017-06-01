3d4be1c92092477391a37250cd34b32bdda96a66
seanbass <seandbass@gmail.com>
2017-05-31 18:32:32 -0700

    changed dht code

--- Software/Python/grove_dht_pro.py
+++ Software/Python/grove_dht_pro.py
@@ -39,20 +39,21 @@ import math
 # Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
 # This example uses the blue colored sensor.
 # SIG,NC,VCC,GND
-sensor = 4  # The Sensor goes on digital port 4.
+sensor = 3  # The Sensor goes on digital port 4.
 
 # temp_humidity_sensor_type
 # Grove Base Kit comes with the blue sensor.
 blue = 0    # The Blue colored sensor.
 white = 1   # The White colored sensor.
 
-while True:
-    try:
-        # This example uses the blue colored sensor. 
-        # The first parameter is the port, the second parameter is the type of sensor.
-        [temp,humidity] = grovepi.dht(sensor,blue)  
-        if math.isnan(temp) == False and math.isnan(humidity) == False:
-            print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
+#while True:
+try:
+    # This example uses the blue colored sensor. 
+    # The first parameter is the port, the second parameter is the type of sensor.
+    [temp,humidity] = grovepi.dht(sensor,1)
+    temp = temp * 1.8 + 32
+    #print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
+    print('{"hum":"%.02f","temp":"%.02f"}'%(humidity,temp))
 
-    except IOError:
-        print ("Error")
+except IOError:
+    print ("Error")
